f91b85c1abfb4e67acb7746eb945a48ca5a9f9b9
HaleySiharath@gmail.com <haley.siharat@gmail.com>
2023-10-10 15:03:34 -0400

    Cluster algorithm centroid calculatons

--- ITCS 5236 Project/Assets/Scripts/Enemy/Behavior/Cluster.cs
+++ ITCS 5236 Project/Assets/Scripts/Enemy/Behavior/Cluster.cs
@@ -29,61 +29,54 @@ public class Cluster : MonoBehaviour
     private void ClusterBehavior(List<GameObject> objects, int numberOfClusters) {
 
 
-        // declare min and max of random positions on world screen
+        // CREATE N CENTROIDS AND RANDOMLY SET THEIR VALUES
+
+        // declare max of random positions on world screen
         int x_max = 1000;
         int y_max = 1000;
 
-        // create random centroids and store to a list
         List<Vector3> centroids = new List<Vector3>();
+
         for(int i = 0; i < numberOfClusters; i++) {
-            centroids.Add(new Vector3(Random.value * x_max,Random.value * y_max, 0.0f));
+            centroids.Add(new Vector3(Random.value * x_max, Random.value * y_max, 0.0f));
         }
 
-        // Create lists for each cluster (each index correlates to centroid)
+        // CREATE A LIST THAT HOLDS EACH CLUSTER (EACH INDEX CORRELATES TO CENTROIDS)
         List<List<GameObject>> clusters = new List<List<GameObject>>();
         for(int i = 0; i < numberOfClusters; i++) {
             clusters.Add(new List<GameObject>());
         }
         
-        // for every point [game object] get distance to each centroid
+        // GET THE DISTANCE BETWEEN EVERY POINT [GAME OBJECT] TO EVERY CENTROID
+        // AND STORE POINT TO THE CLOSEST CLUSTER. (CLUSTER N CORRELATES TO CENTROID N).
 
-        float bestDistance;         // keep track of the best distance to a centroid for a point
+        // keep track of closest cluster/centroid
+        float bestDistance;        
         int indexOfBestDistance;
+        
         foreach (GameObject point in objects) {
             bestDistance = Mathf.Infinity;
             indexOfBestDistance = 0;
+
             for (int i = 0; i < numberOfClusters; i++){
-                // get distance between point and centroid
+                // distance between point and centroid i
                 float curDistance = EuclideanDistance(point.transform.position, centroids[i]);
 
-                // if current distance is better best distance than change best distance
+                // if current distance is smaller than best distance than change best distance
                 if(curDistance < bestDistance) {
                     bestDistance = curDistance;
                     indexOfBestDistance = i;
                 }
             }
-            // put point into the cluster closest to
+
+            // Add point into closest cluster
             clusters[indexOfBestDistance].Add(point);
         }
 
-        // find the average points for each centroid/
-        float x_total;
-        float y_total;
-        for (int i = 0; i < numberOfClusters; i++) {
-            x_total = 0;
-            y_total = 1;
-            // for each cluster find the total of all x and y positions of the points
-             foreach (GameObject point in clusters[i]) {
-                x_total += point.transform.position.x;
-                y_total += point.transform.position.y;
-             }
 
-            //average of X and Y of centroid
-            //centroids[i].x = x_total / clusters[i].Count;
-           // centroids[i].y = y_total / clusters[i].Count;
-        }
+        // UPDATE CENTROID POSITION BASE ON AVERAGE POSITION OF POINTS IN CLUSTER
 
-
+        // centroids = UpdateCentroids(centroids, clusters);
     }
 
     // return the distance between an object and a vector
@@ -93,4 +86,31 @@ public class Cluster : MonoBehaviour
 
         return Mathf.Sqrt(x_distance + y_distance);
     }
+
+    // Update each centroid based on the average of the coinciding cluster
+   /* private List<Vector3> UpdateCentroids(List<Vector3> centroids, List<List<GameObject>> clusters) {
+        
+         // find the average points for each centroid
+        float x_total;
+        float y_total;
+
+        // Update every centroid
+        for (int i = 0; i < clusters.Count; i++) {
+            x_total = 0.0f;
+            y_total = 0.0f;
+
+            // Compute the x and y totals positons for each point in a cluster
+             foreach (GameObject point in clusters[i]) {
+                x_total += point.transform.position.x;
+                y_total += point.transform.position.y;
+             }
+
+            // set centroid to average of x and y total of a cluster
+            centroids[i].x = x_total / clusters[i].Count;
+            centroids[i].y = y_total / clusters[i].Count;
+        }
+
+        
+        return centroids;
+    } */
 }
