0cfbe73306b3262292bd4886fce003eb45c9102a
HaleySiharath@gmail.com <haley.siharat@gmail.com>
2023-10-19 12:15:14 -0400

    basic enemy movement

---
+++ ITCS 5236 Project/Assets/Scripts/Enemy/Behavior/EnemyMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // get current 
+    [SerializeField] private Transform myTransform;
+    [SerializeField] private Transform targetTransform;
+
+    // change speed of change
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float radiusOfSat;
+
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // calculate vector from character to target a
+        Vector3 towardsTarget = targetTransform.position - myTransform.position;
+        float angle = Mathf.Atan2(towardsTarget.y, towardsTarget.x) * Mathf.Rad2Deg;
+        myTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+        // If we haven't reached target then move in the direction towards target
+        if(towardsTarget.magnitude > radiusOfSat) {
+            // normalize vector (size of 1)
+            towardsTarget.Normalize();
+            towardsTarget *= maxSpeed;
+
+            // move enemy
+            rb.AddForce(towardsTarget); 
+        }
+        else {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
